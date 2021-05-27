a7a7188ab3c8c77cb6aed110f17f7360cc5b41eb
Samu <77488879+sase93@users.noreply.github.com>
2021-05-27 16:28:03 +0300

    Added fetch from mongoDB to Cards.js

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { CardDeck, Card, ListGroup, ListGroupItem } from "react-bootstrap";
 import CardSingle from "./CardSingle";
 
 const Cards = () => {
+    const [instructors, setInstructors] = useState([{
+        name: null, // If you declare a state as null, you cannot use it for rendering until it becomes defined
+        rank: null,
+        title: null
+    }]);
+
+    useEffect(() => {
+        fetch("//localhost:3001/instructors").then(res => {
+            if(res.ok) {
+                return res.json();
+            }
+        }).then(jsonRes => {
+            setInstructors(jsonRes)
+        });
+    });
+
     return (
         <div>
+            <h3>Ohjaajat</h3>
+                {instructors.map(instructor => 
+                    <div>
+                        <h4>{instructor.name}</h4>
+                        <p>{instructor.rank}</p>
+                        <p>{instructor.title}</p>
+                    </div>
+                )}
+
             <CardDeck>
                 <Card style={{ width: '18rem' }}>
                     <Card.Img variant="top" src="https://source.unsplash.com/HYDUXzWSF5I/1600x900" />
