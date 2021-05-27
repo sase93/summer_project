a002fd7482afa562d3a55464034775a0f9c6a29d
Samu <77488879+sase93@users.noreply.github.com>
2021-05-27 16:08:52 +0300

    Specified fetch URL more to fix invalid JSON error. MongoDB items now mapped successfully on Instructors page.

--- backend_routes/instructorRoute.js
+++ backend_routes/instructorRoute.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const Instructor = require("../backend_models/instructorModel");
 
 router.route("/add").post((req, res) => {
-    console.log('attempt add');
     const name = req.body.name;
     const rank = req.body.rank;
     const title = req.body.title;
@@ -12,9 +11,8 @@ router.route("/add").post((req, res) => {
         rank,
         title
     });
-    console.log('newInstructor OK');
+
     newInstructor.save();
-    console.log('newInstructor saved');
 });
 
 router.route("/instructors").get((req, res) => {
